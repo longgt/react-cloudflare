e333ecb40d54b7408e9e0c4ee9cc855ed3a30b22
longgt <gtlong.hcmus@gmail.com>
2022-12-20 18:50:41 +0700

    feat: add demo for react-enhance-memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withMemo } from 'react-enhance-memo'
 import './App.css';
+
+type ChildProps = {
+  getCounter: () => number
+}
+
+const ChildComponent = (props: ChildProps) => {
+  const { getCounter } = props
+  const counter = getCounter()
+  const [parentCounter, setParentCounter] = React.useState(counter)
+  const ref = React.useRef<number>(-1)
+  ref.current++
+
+  const checkParentCounter = () => {
+    setParentCounter(getCounter())
+  }
+
+  return (<React.Fragment>
+      <div>
+        <div>Counter: {counter}</div>
+        <div>Parent counter: {parentCounter}</div>
+        <div>Rendered: {ref.current} times</div>
+        <div>
+          <button onClick={() => checkParentCounter()}>
+            Check parent counter
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+const MemoChildComponent = withMemo(ChildComponent);
+
+const ParentComponent = () => {
+  const [counter, setCounter] = React.useState(0)
+
+  const getCounter = React.useCallback(() => {
+    console.log(`Current counter: ${counter}`)
+    return counter
+  }, [counter])
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevState) => prevState + 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  return (
+    <React.Fragment>
+      <h1>Parent component increases counter every second</h1>
+      <div className={'child'}>
+        <div className={'title'}>Child component (Re-renders every second along with parent)</div>
+        <ChildComponent getCounter={getCounter} />
+      </div>
+      <hr />
+      <div className={'child'}>
+        <div className={'title'}>Memo child component (Re-renders after clicking on [Check parent counter] button only)</div>
+        <MemoChildComponent getCounter={getCounter} />
+      </div>
+    </React.Fragment>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParentComponent />
   );
 }
 
